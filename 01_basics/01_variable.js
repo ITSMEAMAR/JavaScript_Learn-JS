9363b91385f66ad6a47a9204b019efc1356c7cb1
AMAR KUMAR <61696079+ITSMEAMAR@users.noreply.github.com>
2025-02-27 19:13:24 +0000

    Variable and constraints

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,13 @@
+const accountID =1234
+let email="abc@sjdh.com"  // always use Const for fix value, and use let for declare
+var accountpass="12323"   // Pls don't use the var because of issue in block issue and functional issue({})
+accountcity = "Delhi"   // we can declear like this also 
+
+let accountstate; // Js will store this value as undefined 
+//accountID=123
+email="abz@gmail.com"
+accountpass ="12334"
+accountcity="Bihar"
+console.log(accountID);
+console.table([accountID, email, accountpass, accountcity, accountstate])
+
