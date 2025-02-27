fd22852817ed2c0f186ff32c90b1802baa70b9a5
AMAR KUMAR <61696079+ITSMEAMAR@users.noreply.github.com>
2025-02-27 19:32:51 +0000

    DataTypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,15 @@
+"use strict"; //treat all JS code as newer version
+
+//alert(3+3) We are using NODe.JS not a browser
+// we can use ; or no need to use the ;, Just use the next line that will work
+
+let name ="Amar" // string data types, can be used in ' ' but Better to use in " "
+let age =25 // number till range of 2 to the power of 53
+let isnumber=true // booliean
+let state =null// null- standlon value, it's representation as value is defined as null
+let num;// undefined- while we don't define any value
+    //symbol= uniques
+
+    //object
+ console.log(typeof name); //check what is the datatypes 
+    
