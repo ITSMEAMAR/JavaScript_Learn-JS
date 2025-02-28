3b349bd34fada1653456d34d5fe154daafcf17ad
AMAR KUMAR <61696079+ITSMEAMAR@users.noreply.github.com>
2025-02-28 15:48:17 +0000

    converios

--- 01_basics/03_converionoperation.js
+++ 01_basics/03_converionoperation.js
@@ -8,4 +8,23 @@ let valueInNumber = Number(score)
 console.log(typeof valueInNumber);
 console.log(valueInNumber); // if we are giving only number then it will print the number as 33 but currently we have string and integer that's why it's ginving NAN
 
+let scores=null  //
+console.log(typeof scores);
+console.log(typeof (scores));
 
+let valueInNumbers = Number(scores)
+
+console.log(typeof valueInNumbers);
+console.log(valueInNumbers); // if we are giving only number then it will print the number as 33 but currently we have string and integer that's why it's ginving NAN
+
+
+let isLoggedIn=1
+let booleanIsLoggedIn=Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn)
+// 1--> true   0-->false
+//""--> false, "abc"=true
+
+let num=233
+let somnum=String(num)
+console.log(somnum);
+console.log(typeof somnum);
---
+++ 01_basics/04_operatio.js
@@ -0,0 +1,26 @@
+let value=2
+let negval=-value
+
+//console.log(negval);
+/*
+
+console.log(2-2);
+console.log(2+2);
+console.log(2*2);
+console.log(2%2);
+console.log(2/2);
+console.log(2**2); */
+
+let str="Amar"
+let str2="Kumar"
+
+//console.log(str+str2); // adding of string
+
+//console.log("1"+2); //taking both as string addition
+
+//console.log("1"+2+3);// string addition
+
+//console.log(1+2+"3"); // taking starting as integer value after addding frist two number it found one sting so based on pervious it added
+
+   
+
