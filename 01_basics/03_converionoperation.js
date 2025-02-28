d69e1cb9c46f680be96b9fdb5041079ff7877d32
AMAR KUMAR <61696079+ITSMEAMAR@users.noreply.github.com>
2025-02-28 14:06:53 +0000

    conversion

---
+++ 01_basics/03_converionoperation.js
@@ -0,0 +1,11 @@
+let score="33abc"  // if we are giving only number then it will print the number
+
+console.log(typeof score);
+console.log(typeof (score));
+
+let valueInNumber = Number(score)
+
+console.log(typeof valueInNumber);
+console.log(valueInNumber); // if we are giving only number then it will print the number as 33 but currently we have string and integer that's why it's ginving NAN
+
+
