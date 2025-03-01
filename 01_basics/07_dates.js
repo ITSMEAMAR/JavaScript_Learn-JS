7d17059858a1d17e28434988775961010089578d
AMAR KUMAR <61696079+ITSMEAMAR@users.noreply.github.com>
2025-03-01 12:16:01 +0000

    date

---
+++ 01_basics/07_dates.js
@@ -0,0 +1,48 @@
+//Date
+/*
+let mydate=new Date()
+console.log(mydate); //date print
+console.log(mydate.toString()); //PRINT IN STRING
+console.log(mydate.toDateString()); //prints only date in string
+console.log(mydate.toISOString());  //stabdard method
+console.log(mydate.toLocaleDateString()); //local formate
+console.log(mydate.toLocaleString());   //local formate of date and time
+
+console.log(mydate.toLocaleTimeString()); //print only time in locat formate
+console.log(mydate.toTimeString()); //will print only time
+console.log(typeof mydate);
+
+
+const mydate1=new Date(2025,02,01) // date is object, Js month is starting from 0-jan to 11- dec
+console.log(mydate1.toDateString());
+
+const mydate2=new Date("2025-04-01")        //date formate is changed when we enter it in sting 
+console.log(mydate2.toLocaleString());  //month will start from01-jan to 12-dec
+
+let mydatestamp= Date.now()
+console.log(mydatestamp);   //proivde the milesseconds
+console.log(mydate2.getTime()); //comapre the time 
+console.log(Math.floor(mydatestamp/1000));  */
+
+
+let newdate=new Date()
+console.log(newdate);
+console.log(newdate.getMonth());    // check which month 
+console.log(newdate.getDay());  //check which day it is mon-01 to sunday-07
+console.log();
+
+
+
+
+
+
+
+
+
+
+
+
+
+ 
+
+
---
+++ 01_basics/07_number_math.js
@@ -0,0 +1,44 @@
+const score=400
+//console.log(score);
+
+const balance=new Number(100)
+//console.log(balance);
+//console.log(typeof balance);
+
+
+
+//console.log(balance.toString().length);
+
+//console.log(balance.toFixed(3)); //print value in flot
+
+const num1=23.4556
+//console.log(num1.toPrecision(5)); //roundoff
+
+const num2=100000000
+//console.log(num2.toLocaleString('ne-IN')); //set number system
+
+//-------------------------------math-----------------------------
+
+
+
+console.log(Math.abs(-4));  //absolute value
+console.log(Math.round(4.6));   //round value
+console.log(Math.ceil(4.2));    //higher value
+console.log(Math.floor(4.2));   //lower value
+
+console.log(Math.sqrt(66)); //sqareroot
+console.log(Math.pow(3,4)); //power value
+console.log(Math.min(4,6,5,3)); //minimum number in list
+console.log(Math.max(1,5,8,9,12)); //maximum number in list
+
+console.log((Math.random()*10)+1);//random will give you 0-1 value randomally
+
+
+
+
+
+
+
+
+
+
